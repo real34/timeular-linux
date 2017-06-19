9760d204681cb94768cedca331f96304e38b20e6
Pierre Martin <pierre@occitech.fr>
2017-06-20 00:24:37 +0200

    Listen to current position

--- src/index.js
+++ src/index.js
@@ -17,6 +17,13 @@ const quit = message => {
   process.exit(0);
 };
 
+const onPositionDetected = newPosition => {
+  const positionKey = Object.keys(Zei.POSITION).find(
+    key => Zei.POSITION[key] == newPosition
+  );
+  console.log(positionKey);
+};
+
 // https://developer.apple.com/library/content/documentation/NetworkingInternetWeb/Conceptual/CoreBluetooth_concepts/CoreBluetoothOverview/CoreBluetoothOverview.html#//apple_ref/doc/uid/TP40013257-CH2-SW1
 noble.on("discover", peripheral => {
   console.log("peripheral discovered");
@@ -25,31 +32,51 @@ noble.on("discover", peripheral => {
   }
 
   console.log("It's a ZEI°!", peripheral);
-  noble.stopScanning(); // see https://github.com/sandeepmistry/noble/issues/165
-
-  console.log("Connecting to", peripheral.id);
-  peripheral.on("disconnect", () => quit("disconnected"));
-  peripheral.connect(err => {
-    console.log("Connected", err);
-    if (err) {
-      quit(err);
-    }
-
-    console.log(
-      "Discovering all services and characteristices of",
-      peripheral.id
-    );
-    peripheral.discoverAllServicesAndCharacteristics(
-      (err, services, characteristics) => {
-        if (err) {
-          quit(err);
-        }
-
-        console.log("Services found:", services);
-        console.log("Characteristics found:", characteristics);
-
-        quit("TODO Do something now!");
+  // see https://github.com/sandeepmistry/noble/issues/165
+  noble.stopScanning(() => {
+    console.log("Connecting to", peripheral.id);
+    peripheral.on("disconnect", () => quit("disconnected"));
+    peripheral.connect(err => {
+      console.log("Connected", err);
+      if (err) {
+        quit(err);
       }
-    );
+
+      console.log(
+        "Discovering all services and characteristices of",
+        peripheral.id
+      );
+      peripheral.discoverSomeServicesAndCharacteristics(
+        [Zei.SERVICE.Orientation],
+        [Zei.CHARACTERISTIC.Position],
+        (err, services, characteristics) => {
+          if (err) {
+            quit(err);
+          }
+
+          console.log("Services found:", services);
+          console.log("Characteristics found:", characteristics);
+
+          const positionCharacteristic = characteristics.find(
+            characteristic =>
+              characteristic.uuid === Zei.CHARACTERISTIC.Position
+          );
+
+          positionCharacteristic.on("data", (data, isNotification) => {
+            console.log(
+              `${isNotification ? "NEW" : "INITIAL"} POSITION:`,
+              data,
+              isNotification
+            );
+            onPositionDetected(data.readUInt8(0));
+          });
+
+          positionCharacteristic.subscribe();
+
+          // TODO Cleanup?
+          // process.on("exit", () => peripheral.disconnect());
+        }
+      );
+    });
   });
 });
--- src/zei.js
+++ src/zei.js
@@ -1,6 +1,53 @@
 const isZEIPeripheral = peripheral =>
   peripheral.advertisement.localName === "Timeular ZEI";
 
+// TODO Convert UUID16 to correct value: see https://stackoverflow.com/a/36212021
+const SERVICE = {
+  Orientation: "c7e70010c84711e681758c89a55d403c" //"0x0010"
+};
+
+const CHARACTERISTIC = {
+  Accelerometer: "c7e70011c84711e681758c89a55d403c", //"0x0011",
+  Position: "c7e70012c84711e681758c89a55d403c", //"0x0012",
+  PushButton: "c7e70021c84711e681758c89a55d403c", //"0x0021",
+  LED: "c7e70022c84711e681758c89a55d403c" //"0x0022"
+};
+
+const POSITION = {
+  TIP_UP: 0,
+  DOWN_NORTH: 1,
+  DOWN_WEST: 2,
+  DOWN_SOUTH: 3,
+  DOWN_EAST: 4,
+  UP_NORTH: 5,
+  UP_WEST: 6,
+  UP_SOUTH: 7,
+  UP_EAST: 8,
+  TIP_DOWN: 9
+};
+
+const PUSH_TYPE = {
+  SHORT: 0,
+  LONG: 1
+};
+
+const LED_COLOR = {
+  RED: 0,
+  GREEN: 1,
+  BLUE: 2
+};
+
+const DURATION = {
+  SHORT: 0,
+  LONG: 1
+};
+
 module.exports = {
-  isZEIPeripheral
+  isZEIPeripheral,
+  SERVICE,
+  CHARACTERISTIC,
+  POSITION,
+  PUSH_TYPE,
+  LED_COLOR,
+  DURATION
 };
